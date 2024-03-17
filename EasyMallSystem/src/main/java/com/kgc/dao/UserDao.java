243a9f926bff6c7fb2517f2c5888dafe37cd5959
ouyanghong <2358560084@qq.com>
2024-03-17 19:32:40 +0800

    易买网后端项目架构搭建0.0.1base版

---
+++ EasyMallSystem/src/main/java/com/kgc/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.kgc.dao;
+
+public interface UserDao {
+
+}
---
+++ EasyMallSystem/src/main/java/com/kgc/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.kgc.service.impl;
+
+import com.kgc.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: 欧洋宏
+ * @create: 2024-03-17 19:22
+ **/
+@Service
+public class UserServiceImpl implements UserService {
+    private Logger logger = Logger.getLogger(getClass());
+
+}
