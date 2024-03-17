39c096bd5dbcbd393085bd9aec37054f137b82b8
ouyanghong <2358560084@qq.com>
2024-03-17 19:29:04 +0800

    易买网后端项目架构搭建0.0.1base版

---
+++ EasyMallSystem/src/main/java/com/kgc/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.kgc.controller;
+
+import com.kgc.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: 欧洋宏
+ * @create: 2024-03-17 19:17
+ **/
+@RestController
+public class UserController {
+    private Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("test")
+    public String test() {
+        return "test";
+    }
+}
