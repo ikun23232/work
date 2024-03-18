cd985e9387d7fb01f28495d92678d63e2b92b40f
ouyanghong <2358560084@qq.com>
2024-03-18 16:28:27 +0800

    易买网后端项目架构搭建 (登录+注册+新闻+分类+忘记密码)

--- EasyMallSystem/src/main/java/com/kgc/dao/ProductDao.java
+++ EasyMallSystem/src/main/java/com/kgc/dao/ProductDao.java
@@ -1,4 +1,9 @@
 package com.kgc.dao;
 
+import com.kgc.entity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+    public List<Product> getProductDao();
 }
--- EasyMallSystem/src/main/java/com/kgc/service/impl/ProductServiceImpl.java
+++ EasyMallSystem/src/main/java/com/kgc/service/impl/ProductServiceImpl.java
@@ -1,8 +1,19 @@
 package com.kgc.service.impl;
 
+import com.kgc.dao.ProductDao;
+import com.kgc.service.ProductService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author: 欧洋宏
  * @create: 2024-03-18 15:50
  **/
-public class ProductServiceImpl {
+@Service
+public class ProductServiceImpl implements ProductService {
+    private Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private ProductDao productDao;
+
 }
