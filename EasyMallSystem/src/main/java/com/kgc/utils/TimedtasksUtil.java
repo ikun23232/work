d6e1c0fb6f80bab13909bd39fde398b3c275f727
ouyanghong <2358560084@qq.com>
2024-03-27 03:23:10 +0800

    易买网后端项目架构搭建 3.27 19:50 end 分类管理 购物车 提交订单 咨询
    未改重放攻击版 未做登录权限版本

--- EasyMallSystem/src/main/java/com/kgc/utils/TimedtasksUtil.java
+++ EasyMallSystem/src/main/java/com/kgc/utils/TimedtasksUtil.java
@@ -1,9 +1,7 @@
 package com.kgc.utils;
 
-import com.kgc.entity.Message;
 import com.kgc.entity.Order;
 import com.kgc.service.OrderService;
-import com.kgc.service.impl.OrderServiceImpl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -25,11 +23,11 @@ public class TimedtasksUtil {
     public TimedtasksUtil() {
         this.executor = Executors.newScheduledThreadPool(1);
     }
-    public void startOrderTimeoutTask(String orderId, long createTime) {
+    public void startOrderTimeoutTask(String orderId, long createTime,int userId) {
         long delay = 10 * 1 * 1000; // 30分钟的延迟时间
-        executor.schedule(() -> checkAndDeleteOrder(orderId, createTime), delay, TimeUnit.MILLISECONDS);
+        executor.schedule(() -> checkAndDeleteOrder(orderId, createTime,userId), delay, TimeUnit.MILLISECONDS);
     }
-    private void checkAndDeleteOrder(String orderId, long createTime) {
+    private void checkAndDeleteOrder(String orderId, long createTime, int userId) {
         // 在这里编写检查订单并删除的逻辑\
         //直接删除我的订单
         System.out.println("我来了.................");
@@ -41,12 +39,10 @@ public class TimedtasksUtil {
         Order orderById = orderService.getOrderById(orderIdTemp);
         //订单状态为未支付直接删除回退库存数量
         if (orderById.getStatus()!=1){
-            orderService.delOrderById(Integer.parseInt(orderId));
+            orderService.delOrderByIdAndUserId(Integer.parseInt(orderId),userId);
         }
         System.out.println("定时器来呐");
         //判断订单状态如果是1我就不执行
-
-
 
     }
     public void shutdown() {
